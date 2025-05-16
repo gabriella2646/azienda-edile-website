b809e64e883b72fe7dfb59358b1ea50f04e873d4
gabriella2646 <gabryella.ispi@gmail.com>
2025-05-16 17:00:49 +0200

    Add .next/static/chunks/src_app_preventivi_page_tsx_206f9780._.js 1747407649020

---
+++ .next/static/chunks/src_app_preventivi_page_tsx_206f9780._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_preventivi_page_tsx_692f0e7e._.js"
+],
+    source: "dynamic"
+});
