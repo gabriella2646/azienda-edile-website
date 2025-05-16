55f56264ba5af7b80102bd16c494dc592d748144
gabriella2646 <gabryella.ispi@gmail.com>
2025-05-16 17:01:13 +0200

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747407672472

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__7db745e8._.css",
+  "static/chunks/node_modules_next_dist_1775bd72._.js",
+  "static/chunks/src_components_Navbar_tsx_624fc2ba._.js"
+],
+    source: "dynamic"
+});
